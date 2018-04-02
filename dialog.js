4c6e4f6df930fadb4596773d5a0747e038607e51
sulishibaobei <380571249@qq.com>
2018-04-02 11:14:34 +0800

    v1.1.2新增一个按钮时毁掉事件

--- dialog.js
+++ dialog.js
@@ -1,5 +1,8 @@
-(function (window){
-    var btnsure='',btncancel='',width,content,time;
+(function(window) {
+    var btnsure = '',
+        btncancel = '',
+        width, content, time;
+
     function posContentDialog(obj) {
         var clientX = document.body.clientWidth;
         var clientY = document.body.clientHeight;
@@ -20,47 +23,56 @@
             top: (clientY - height) / 2
         })
     }
+
     function Sure(callback) {
         $('body').on('click', '.sure', function() {
             callback();
             $(this).parents('.dialog').remove()
         })
     }
+
     function Cancel(callback) {
         $('body').on('click', '.cancel', function() {
-            callback();
+            if (callback != false) {
+                if (!callback()) {
+                    callback();
+                }
+            }
             $(this).parents('.dialog').remove()
         })
     }
+
     function Cancel1() {
         $('body').on('click', '.cancel1', function() {
             $(this).parents('.dialog').remove()
         })
     }
+
     function lineHeight(obj) {
         obj.css('line-height', obj.height() + 'px')
     }
-
-    class dialog{
-        constructor(content, width, btnSure, btnCancel,time){
-            this.content=content
-            this.width=width
-            this.btnSure=btnSure
-            this.btnCancel=btnCancel
-            this.time=time
+    class dialog {
+        constructor(content, width, btnSure, btnCancel, time) {
+            this.content = content,
+                this.width = width,
+                this.btnSure = btnSure,
+                this.btnCancel = btnCancel,
+                this.time = time
         }
+
         whetherDialog() {
-            if (btnSure) {
+            if (this.btnSure) {
                 btnsure = this.btnSure[0].value;
                 Sure(this.btnSure[0].callback)
             } else {
                 btnsure = '确认'
             }
-            if (btnCancel) {
+            if (this.btnCancel) {
                 btncancel = this.btnCancel[0].value;
                 Cancel(this.btnCancel[0].callback)
             } else {
-                btncancel = '取消'
+                btncancel = '取消';
+                Cancel(false);
             }
             width = (this.width || 300) + 'px';
             var dialog = "<div class='dialog'> <div class='dialog-shadow'></div> <div class='dialog-content' style='width:" + width + "'><div class='title'>" + this.content + "</div><div class='dialog-footer'> <button class='sure'>" + btnsure + "</button><button class='cancel'>" + btncancel + "</button></div></div></div>";
@@ -68,7 +80,6 @@
             var obj = $('body').find('.dialog').last();
             posContentDialog(obj.find('.dialog-content'))
         };
-        
         nobtnDialog() {
             width = (this.width || 150) + 'px';
             var dialog = " <div class='dialog' style='z-index:999'><div class='dialog-shadow'></div> <div class='dialog-content' style='width:" + width + "'>" + this.content + "</div></div>";
@@ -83,40 +94,44 @@
         }
         onebtnDialog() {
             width = (this.width || 300) + 'px';
-            btncancel = this.btnCancel || '关闭';
-            var dialog = " <div class='dialog'><div class='dialog-shadow'></div> <div class='dialog-content' style='width:" + width + "'><div class='title'>" + this.content + "</div><div class='dialog-footer'><button class='cancel1'>" + btncancel + "</button></div></div></div> ";
+            if (typeof this.btnCancel == 'object') {
+                btncancel = this.btnCancel[0].value;
+                Cancel(this.btnCancel[0].callback)
+            } else {
+                btncancel = this.btnCancel || '关闭';
+                Cancel(false);
+            }
+            var dialog = " <div class='dialog'><div class='dialog-shadow'></div> <div class='dialog-content' style='width:" + width + "'><div class='title'>" + this.content + "</div><div class='dialog-footer'><button class='cancel'>" + btncancel + "</button></div></div></div> ";
             $('body').append(dialog);
             var obj = $('body').find('.dialog').last();
             posContentDialog(obj.find('.dialog-content'));
-            Cancel1();
-            if(this.time){
+            if (this.time) {
                 time = this.time || 1500;
                 setTimeout(function() {
                     obj.remove()
-                }, time) 
+                }, time)
             }
         }
     }
-    window.dialog={
-      twoBtnDialog:function(content, width, btnSure, btnCancel){
-          new dialog(content, width, btnSure, btnCancel).whetherDialog();
-      },
-      oneBtnDialog:function(content, width,btnSure,btnCancel, time){
-        new dialog(content, width, btnCancel, time).onebtnDialog();
-      },
-      noBtnDialog:function(content, width,btnSure,btnCancel, time){
-          new dialog(content, width, time).nobtnDialog();
-      }
+    window.dialog = {
+        twoBtnDialog: function(content, width, btnSure, btnCancel) {
+            new dialog(content, width, btnSure, btnCancel).whetherDialog()
+        },
+        oneBtnDialog: function(content, width, btnCancel, time) {
+            new dialog(content, width, '', btnCancel, time).onebtnDialog()
+        },
+        noBtnDialog: function(content, width, time) {
+            new dialog(content, width, '', '', time).nobtnDialog()
+        }
+    }, window.onresize = function() {
+        posContentDialog($('.dialog-content'))
+    }, window.onscroll = function() {
+        var clientX = document.body.scrollWidth;
+        var clientY = document.body.scrollHeight;
+        $(".dialog-shadow").css({
+            'width': clientX,
+            'height': clientY
+        });
+        posContentDialog($('.dialog-content'))
     }
-    window.onresize = function () { //改变大小的时候大小重置
-        posContentDialog($('.dialog-content'));
-    }
-    window.onscroll = function(){
-        var clientX= document.body.scrollWidth;  //网页可见区域的宽高
-        var clientY=  document.body.scrollHeight ; 
-        $(".dialog-shadow").css({'width':clientX,'height':clientY});
-        posContentDialog($('.dialog-content'));
-     };
-     
-    
 }(window))
